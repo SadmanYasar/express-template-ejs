3ac050cae8d935ff11b8482bd87c80e19d682335
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-09-20 23:38:30 +0000

    init

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 let express = require('express');
 let app = express();
 let ejs = require('ejs');
-const haikus = require('./haikus.json');
-const port = process.env.PORT || 3000;
+const settings = require('./settings.json');
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+  res.render('index', { settings });
 });
 
-app.listen(port);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
